69265a84fdbf2f3d3272fff29781cd126bf75dca
TatianaChes <tatianaches49@gmail.com>
2024-09-22 12:01:42 +0500

    commit without database only localStorage

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-Ll0NMqALVHmbEGXCRhiIJK-cGivu1vU",
+  authDomain: "react-notes-b6abf.firebaseapp.com",
+  projectId: "react-notes-b6abf",
+  storageBucket: "react-notes-b6abf.appspot.com",
+  messagingSenderId: "435360286095",
+  appId: "1:435360286095:web:3841201c454c08ae4677fa",
+  measurementId: "G-X50RMDV37H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app)
+const notesCollection = collection(db, "notes")
